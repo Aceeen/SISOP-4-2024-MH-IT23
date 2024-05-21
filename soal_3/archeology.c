af3579fe2087fdf79145c9128f085571348b9852
v0rein <143814923+v0rein@users.noreply.github.com>
2024-05-21 17:45:55 +0700

    Update archeology.c
    nambahin beberapa debugging

--- soal_3/archeology.c
+++ soal_3/archeology.c
@@ -33,7 +33,9 @@ int fuze_getattr(const char *path, struct stat *statbuf)
         return 0;
     }
     char full_path[MAX_BUFFER];
-    sprintf(full_path, "%s%s", source_path, path);
+    snprintf(full_path, MAX_BUFFER, "%s%s", source_path, path);
+    printf("Full path: %s\n", full_path);
+
     statbuf->st_mode = S_IFREG | 0644;
     statbuf->st_nlink = 1;
     statbuf->st_size = 0;
@@ -42,19 +44,24 @@ int fuze_getattr(const char *path, struct stat *statbuf)
     int i = 0;
     while (1)
     {
-        sprintf(apath, "%s.%03d", full_path, i++);
+        snprintf(apath, MAX_BUFFER + 4, "%s.%03d", full_path, i++);
+        printf("Checking fragment: %s\n", apath);
         fd = fopen(apath, "rb");
         if (!fd)
+        {
+            if (i == 1)
+            {
+                printf("File not found: %s\n", apath);
+                printf("Leaving fuze_getattr with error for %s\n", path);
+                return -ENOENT;
+            }
             break;
+        }
         fseek(fd, 0L, SEEK_END);
         statbuf->st_size += ftell(fd);
         fclose(fd);
     }
-    if (i == 1)
-    {
-        printf("Leaving fuze_getattr with error for %s\n", path);
-        return -errno;
-    }
+    printf("File size: %ld\n", statbuf->st_size);
     printf("Leaving fuze_getattr for %s\n", path);
     return 0;
 }
@@ -67,12 +74,15 @@ int fuze_readdir(const char *path, void *buffer, fuse_fill_dir_t filler, off_t o
     filler(buffer, ".", NULL, 0);
     filler(buffer, "..", NULL, 0);
     char full_path[MAX_BUFFER];
-    sprintf(full_path, "%s%s", source_path, path);
+    snprintf(full_path, MAX_BUFFER, "%s%s", source_path, path);
+    printf("Full path: %s\n", full_path);
+
     DIR *dirpath = opendir(full_path);
     struct dirent *de;
     struct stat st;
     if (!dirpath)
     {
+        printf("Could not open directory: %s\n", full_path);
         printf("Leaving fuze_readdir with error for %s\n", path);
         return -errno;
     }
@@ -99,7 +109,9 @@ int fuze_read(const char *path, char *buffer, size_t sz, off_t os, struct fuse_f
 {
     printf("Entering fuze_read for %s\n", path);
     char full_path[MAX_BUFFER];
-    sprintf(full_path, "%s%s", source_path, path);
+    snprintf(full_path, MAX_BUFFER, "%s%s", source_path, path);
+    printf("Full path: %s, size: %zu, offset: %ld\n", full_path, sz, os);
+
     char apath[MAX_BUFFER + 4];
     FILE *fdir;
     int i = 0;
@@ -107,10 +119,14 @@ int fuze_read(const char *path, char *buffer, size_t sz, off_t os, struct fuse_f
     size_t sum_read = 0;
     while (sz > 0)
     {
-        sprintf(apath, "%s.%03d", full_path, i++);
+        snprintf(apath, MAX_BUFFER + 4, "%s.%03d", full_path, i++);
+        printf("Reading from fragment: %s\n", apath);
         fdir = fopen(apath, "rb");
         if (!fdir)
+        {
+            printf("Could not open fragment: %s\n", apath);
             break;
+        }
         fseek(fdir, 0L, SEEK_END);
         size_t sz_part = ftell(fdir);
         fseek(fdir, 0L, SEEK_SET);
@@ -138,7 +154,9 @@ int fuze_write(const char *path, const char *buffer, size_t sz, off_t os, struct
 {
     printf("Entering fuze_write for %s\n", path);
     char full_path[MAX_BUFFER];
-    sprintf(full_path, "%s%s", source_path, path);
+    snprintf(full_path, MAX_BUFFER, "%s%s", source_path, path);
+    printf("Full path: %s, size: %zu, offset: %ld\n", full_path, sz, os);
+
     char apath[MAX_BUFFER + 4];
     FILE *fdir;
     int pcur = os / MAX_SPLIT;
@@ -146,28 +164,26 @@ int fuze_write(const char *path, const char *buffer, size_t sz, off_t os, struct
     size_t sum_write = 0;
     while (sz > 0)
     {
-        sprintf(apath, "%s.%03d", full_path, pcur++);
+        snprintf(apath, MAX_BUFFER + 4, "%s.%03d", full_path, pcur++);
+        printf("Writing to fragment: %s\n", apath);
         fdir = fopen(apath, "r+b");
         if (!fdir)
         {
             fdir = fopen(apath, "wb");
             if (!fdir)
             {
+                printf("Could not create fragment: %s\n", apath);
                 printf("Leaving fuze_write with error for %s\n", path);
                 return -errno;
             }
         }
         fseek(fdir, pos, SEEK_SET);
-        size_t sz_write;
-        if (sz > (MAX_SPLIT - pos))
-            sz_write = MAX_SPLIT - pos;
-        else
-            sz_write = sz;
+        size_t sz_write = (sz > (MAX_SPLIT - pos)) ? (MAX_SPLIT - pos) : sz;
         printf("Writing %zu bytes to %s\n", sz_write, apath);
-        fwrite(buffer, 1, sz_write, fdir); // Tambahkan tanda titik koma di sini ;
+        fwrite(buffer, 1, sz_write, fdir);
         fclose(fdir);
         buffer += sz_write;
-        sz -= sz_write; // Ganti size dengan sz
+        sz -= sz_write;
         sum_write += sz_write;
         pos = 0;
     }
@@ -180,10 +196,13 @@ int fuze_create(const char *path, mode_t md, struct fuse_file_info *fi)
     printf("Entering fuze_create for %s\n", path);
     (void)fi;
     char full_path[MAX_BUFFER];
-    sprintf(full_path, "%s%s.000", source_path, path);
+    snprintf(full_path, MAX_BUFFER, "%s%s.000", source_path, path);
+    printf("Creating file: %s\n", full_path);
+
     int rez = creat(full_path, md);
     if (rez == -1)
     {
+        printf("Could not create file: %s\n", full_path);
         printf("Leaving fuze_create with error for %s\n", path);
         return -errno;
     }
@@ -194,92 +213,116 @@ int fuze_create(const char *path, mode_t md, struct fuse_file_info *fi)
 
 int fuze_unlink(const char *path)
 {
+    printf("Entering fuze_unlink for %s\n", path);
     char full_path[MAX_BUFFER];
     char apath[MAX_BUFFER + 4];
-    sprintf(full_path, "%s%s", source_path, path);
+    snprintf(full_path, MAX_BUFFER, "%s%s", source_path, path);
+    printf("Full path: %s\n", full_path);
 
     int pcur = 0;
     while (1)
     {
-        sprintf(apath, "%s.%03d", full_path, pcur++);
+        snprintf(apath, MAX_BUFFER + 4, "%s.%03d", full_path, pcur++);
+        printf("Unlinking fragment: %s\n", apath);
         int rez = unlink(apath);
         if (rez == -1)
         {
             if (errno == ENOENT)
                 break;
+            printf("Could not unlink fragment: %s\n", apath);
+            printf("Leaving fuze_unlink with error for %s\n", path);
             return -errno;
         }
     }
+    printf("Leaving fuze_unlink for %s\n", path);
     return 0;
 }
 
 int fuze_truncate(const char *path, off_t sz)
 {
+    printf("Entering fuze_truncate for %s\n", path);
     char full_path[MAX_BUFFER];
     char apath[MAX_BUFFER + 4];
-    sprintf(full_path, "%s%s", source_path, path);
-    int pcurrent_t = 0;
+    snprintf(full_path, MAX_BUFFER, "%s%s", source_path, path);
+    printf("Full path: %s, size: %ld\n", full_path, sz);
+
+    int pcurr_t = 0;
     size_t sz_part;
     off_t sz_rmn = sz;
     while (sz_rmn > 0)
     {
-        sprintf(apath, "%s.%03d", full_path, pcurrent_t++);
+        snprintf(apath, MAX_BUFFER + 4, "%s.%03d", full_path, pcurr_t++);
         if (sz_rmn > MAX_SPLIT)
             sz_part = MAX_SPLIT;
         else
             sz_part = sz_rmn;
+        printf("Truncating fragment: %s to size: %zu\n", apath, sz_part);
         int rez = truncate(apath, sz_part);
         if (rez == -1)
+        {
+            printf("Could not truncate fragment: %s\n", apath);
+            printf("Leaving fuze_truncate with error for %s\n", path);
             return -errno;
+        }
         sz_rmn -= sz_part;
     }
-    int pcurrent_u = 0;
+    int pcurr_u = 0;
     while (1)
     {
-        sprintf(apath, "%s.%03d", full_path, pcurrent_u++);
+        snprintf(apath, MAX_BUFFER + 4, "%s.%03d", full_path, pcurr_u++);
         int rez = unlink(apath);
         if (rez == -1)
         {
             if (errno == ENOENT)
                 break;
+            printf("Could not unlink fragment: %s\n", apath);
+            printf("Leaving fuze_truncate with error for %s\n", path);
             return -errno;
         }
     }
+    printf("Leaving fuze_truncate for %s\n", path);
     return 0;
 }
 
 int fuze_utimens(const char *path, const struct timespec tspec[2])
 {
+    printf("Entering fuze_utimens for %s\n", path);
     char full_path[MAX_BUFFER];
-    sprintf(full_path, "%s%s", source_path, path);
+    snprintf(full_path, MAX_BUFFER, "%s%s", source_path, path);
+    printf("Full path: %s\n", full_path);
+
     char apath[MAX_BUFFER + 4];
-    int pcurrent = 0;
+    int pcurr = 0;
     while (1)
     {
-        sprintf(apath, "%s.%03d", full_path, pcurrent++);
+        snprintf(apath, MAX_BUFFER + 4, "%s.%03d", full_path, pcurr++);
+        printf("Updating timestamps for fragment: %s\n", apath);
         int res = utimensat(AT_FDCWD, apath, tspec, 0);
         if (res == -1)
         {
             if (errno == ENOENT)
                 break;
+            printf("Could not update timestamps for fragment: %s\n", apath);
+            printf("Leaving fuze_utimens with error for %s\n", path);
             return -errno;
         }
     }
-
+    printf("Leaving fuze_utimens for %s\n", path);
     return 0;
 }
 
 int main(int argc, char *argv[])
 {
-    struct fuse_operations fuze_oper;
-    memset(&fuze_oper, 0, sizeof(struct fuse_operations));
-    fuze_oper.getattr = fuze_getattr;
-    fuze_oper.readdir = fuze_readdir;
-    fuze_oper.read = fuze_read;
-    fuze_oper.write = fuze_write;
-    fuze_oper.create = fuze_create;
-    fuze_oper.unlink = fuze_unlink;
-    fuze_oper.truncate = fuze_truncate;
-    fuze_oper.utimens = fuze_utimens;
+    struct fuse_operations fuze_oper = {
+        .getattr = fuze_getattr,
+        .readdir = fuze_readdir,
+        .read = fuze_read,
+        .write = fuze_write,
+        .create = fuze_create,
+        .unlink = fuze_unlink,
+        .truncate = fuze_truncate,
+        .utimens = fuze_utimens,
+    };
+    printf("Starting FUSE filesystem\n");
     return fuse_main(argc, argv, &fuze_oper, NULL);
 }
